dd7b7ef5585ce954de0f673522eb96222c1e0a09
sandeep <sandeepmukherjee12@gmail.com>
2024-05-26 23:24:47 +0530

    Edit Todo

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -1,9 +1,17 @@
-function Todo({ deleteTodo, text, isFinished, id }) {
+import { useState } from "react";
+
+function Todo({ finishTodo, editTodo, deleteTodo, text, isFinished, id }) {
+    const [isEditing, setIdEditing] = useState(false);
+    const [todoText, setTodoText] = useState(text);
+
     return (
         <div>
-            <input type="checkbox" checked={isFinished} />
-            <span>{text}</span>
-            <button>Edit</button>
+            <input type="checkbox" checked={isFinished} onChange={() => finishTodo(!isFinished)} />
+            {isEditing ? <input value={todoText} onChange={(e) => setTodoText(e.target.value)} /> : <span>{todoText}</span>}
+            <button onClick={() => {
+                setIdEditing(!isEditing);
+                editTodo(todoText);
+            }}>{isEditing ? "SAVE" : "EDIT"}</button>
             <button onClick={deleteTodo}>Delete</button>
         </div>
     )
--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -6,6 +6,26 @@ function TodoList({ todos, setTodos }) {
         const newTodolist = todos.filter(todo => todo.id != id);
         setTodos(newTodolist);
     }
+    function onEditTodo(id, newTodo) {
+        const newTodoList = todos.map(todo => {
+            if (todo.id === id) {
+                todo.text = newTodo;
+            }
+            return todo;
+        })
+        setTodos(newTodoList);
+    }
+
+    function onFinishTodo(id, state) {
+        const newTodoList = todos.map(todo => {
+            if (todo.id === id) {
+                todo.isFinished = state;
+            }
+            return todo;
+        })
+        setTodos(newTodoList);
+    }
+
     return (
         todos &&
         todos.map((todo) =>
@@ -13,7 +33,9 @@ function TodoList({ todos, setTodos }) {
                 key={todo.id}
                 text={todo.text}
                 isFinished={todo.isFinished}
+                editTodo={(newTodo) => onEditTodo(todo.id, newTodo)}
                 deleteTodo={() => onDeleteTodo(todo.id)}
+                finishTodo={(state) => onFinishTodo(todo.id, state)}
             />)
     )
 }
